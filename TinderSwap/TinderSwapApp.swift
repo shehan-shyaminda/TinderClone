04d5115efc7091e3f5107769ed250db48a35fa49
shehan-shyaminda <shehan.shyaminda@gmail.com>
2022-05-17 10:35:51 +0530

    Initial Commit

---
+++ TinderSwap/TinderSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwapApp.swift
+//  TinderSwap
+//
+//  Created by STEPHEN INNOVATIONS on 2022-05-17.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
